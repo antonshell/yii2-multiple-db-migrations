140f65ece603deb528c73e3405dd0998e941630f
Anton Shelyganov <antonshell@yandex.ru>
2016-03-20 13:05:54 +0300

    remove db2.config

--- config/db2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_migrations_db2',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
